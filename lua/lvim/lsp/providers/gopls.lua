017a367e13e567dfd9af6e944c6b3397d1ebe74f
Danilo Nascimento <dan.silva.13.zx@gmail.com>
2024-06-16 14:29:30 -0300

    Config GOPLS lsp provider

---
+++ lua/lvim/lsp/providers/gopls.lua
@@ -0,0 +1,13 @@
+local util = require "lspconfig/util"
+return {
+  cmd = { "gopls", "serve" },
+  root_dir = util.root_pattern("go.work", "go.mod", ".git"),
+  settings = {
+    gopls = {
+      analyses = {
+        unusedparams = true,
+      },
+      staticcheck = true,
+    },
+  },
+}
