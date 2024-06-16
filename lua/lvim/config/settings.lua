27c8e4fe0b17bcd3828a4fe290287302c6e4f7f8
Danilo Nascimento <dan.silva.13.zx@gmail.com>
2024-06-16 14:30:10 -0300

    Personal settings

--- lua/lvim/config/settings.lua
+++ lua/lvim/config/settings.lua
@@ -11,56 +11,98 @@ M.load_default_options = function()
   end
 
   local default_options = {
-    backup = false, -- creates a backup file
-    clipboard = "unnamedplus", -- allows neovim to access the system clipboard
-    cmdheight = 1, -- more space in the neovim command line for displaying messages
-    completeopt = { "menuone", "noselect" },
-    conceallevel = 0, -- so that `` is visible in markdown files
-    fileencoding = "utf-8", -- the encoding written to a file
-    foldmethod = "manual", -- folding, set to "expr" for treesitter based folding
-    foldexpr = "", -- set to "nvim_treesitter#foldexpr()" for treesitter based folding
-    hidden = true, -- required to keep multiple buffers and open multiple buffers
-    hlsearch = true, -- highlight all matches on previous search pattern
-    ignorecase = true, -- ignore case in search patterns
-    mouse = "a", -- allow the mouse to be used in neovim
-    pumheight = 10, -- pop up menu height
-    showmode = false, -- we don't need to see things like -- INSERT -- anymore
-    smartcase = true, -- smart case
-    splitbelow = true, -- force all horizontal splits to go below current window
-    splitright = true, -- force all vertical splits to go to the right of current window
-    swapfile = false, -- creates a swapfile
-    termguicolors = true, -- set term gui colors (most terminals support this)
-    timeoutlen = 1000, -- time to wait for a mapped sequence to complete (in milliseconds)
-    title = true, -- set the title of window to the value of the titlestring
-    -- opt.titlestring = "%<%F%=%l/%L - nvim" -- what the title of the window will be set to
-    undodir = undodir, -- set an undo directory
-    undofile = true, -- enable persistent undo
-    updatetime = 100, -- faster completion
-    writebackup = false, -- if a file is being edited by another program (or was written to file while editing with another program), it is not allowed to be edited
-    expandtab = true, -- convert tabs to spaces
-    shiftwidth = 2, -- the number of spaces inserted for each indentation
-    tabstop = 2, -- insert 2 spaces for a tab
-    cursorline = true, -- highlight the current line
-    number = true, -- set numbered lines
-    numberwidth = 4, -- set number column width to 2 {default 4}
-    signcolumn = "yes", -- always show the sign column, otherwise it would shift the text each time
-    wrap = false, -- display lines as one long line
-    shadafile = join_paths(get_cache_dir(), "lvim.shada"),
-    scrolloff = 8, -- minimal number of screen lines to keep above and below the cursor.
-    sidescrolloff = 8, -- minimal number of screen lines to keep left and right of the cursor.
-    showcmd = false,
-    ruler = false,
-    laststatus = 3,
+    opt = {
+      -- opt.titlestring = "%<%F%=%l/%L - nvim" -- what the title of the window will be set to
+      autowrite = true,                             -- Enable auto write
+      backspace = vim.opt.backspace + { "nostop" }, -- Don't stop backspace at insert
+      backup = false,                               -- creates a backup file
+      clipboard = "unnamedplus",                    -- Connection to the system clipboard
+      cmdheight = 0,                                -- hide command line unless needed
+      completeopt = { "menuone", "noselect" },      -- Options for insert mode completion
+      conceallevel = 3,                             -- Hide * markup for bold and italic
+      confirm = true,                               -- Confirm to save changes before exiting modified buffer
+      copyindent = true,                            -- Copy the previous indentation on autoindenting
+      cursorline = true,                            -- highlight the current line
+      expandtab = true,                             -- convert tabs to spaces
+      fileencoding = "utf-8",                       -- the encoding written to a file
+      fillchars = { eob = " " },                    -- Disable `~` on nonexistent lines
+      foldexpr = "",                                -- set to "nvim_treesitter#foldexpr()" for treesitter based folding
+      foldmethod = "manual",                        -- folding, set to "expr" for treesitter based folding
+      formatoptions = "jcroqlnt",                   -- tcqj
+      grepformat = "%f:%l:%c:%m",                   --
+      grepprg = "rg, --vimgrep",                    --
+      guifont = "FiraCodeNerdFontMono",             --
+      hidden = true,                                -- required to keep multiple buffers and open multiple buffers
+      history = 100,                                -- Number of commands to remember in a history table
+      hlsearch = true,                              -- highlight all matches on previous search pattern
+      ignorecase = true,                            -- ignore case in search patterns
+      inccommand = "nosplit",                       -- preview incremental substitute
+      laststatus = 3,                               -- globalstatus
+      list = true,                                  -- Show some invisible characters (tabs...
+      listchars = "tab:→ ,eol:↴,trail:⋅,extends:❯,precedes:❮",
+      mouse = "a",                                  -- allow the mouse to be used in neovim
+      number = true,                                -- set numbered lines
+      numberwidth = 4,                              -- set number column width to 2 {default 4}
+      preserveindent = true,                        -- Preserve indent structure as much as possible
+      pumblend = 10,                                -- Popup blend
+      pumheight = 10,                               -- Height of the pop up menu
+      relativenumber = false,                       -- Not show relative numberline
+      ruler = false,
+      scrolloff = 99999,                            -- Number of lines to keep above and below the cursor
+      sessionoptions = { "buffers", "curdir", "tabpages", "winsize" },
+      shadafile = join_paths(get_cache_dir(), "lvim.shada"),
+      shiftround = true,              -- Keep cursor in middle screen
+      shiftwidth = 2,                 -- the number of spaces inserted for each indentation
+      showcmd = false,
+      showmode = false,               -- we don't need to see things like -- INSERT -- anymore
+      sidescrolloff = 8,              -- minimal number of screen lines to keep left and right of the cursor.
+      signcolumn = "yes",             -- always show the sign column, otherwise it would shift the text each time
+      smartcase = true,               -- smart case
+      smartindent = true,             -- Insert indents automatically
+      spelllang = { "en" },           --
+      splitbelow = true,              -- force all horizontal splits to go below current window
+      splitright = true,              -- force all vertical splits to go to the right of current window
+      swapfile = false,               -- creates a swapfile
+      tabstop = 2,                    -- insert 2 spaces for a tab
+      termguicolors = true,           -- set term gui colors (most terminals support this)
+      timeoutlen = 300,               -- time to wait for a mapped sequence to complete (in milliseconds)
+      -- timeoutlen = 1000,              -- time to wait for a mapped sequence to complete (in milliseconds)
+      title = true,                   -- set the title of window to the value of the titlestring
+      undodir = undodir,              -- set an undo directory
+      undofile = true,                -- enable persistent undo
+      undolevels = 10000,             --
+      updatetime = 100,               -- faster completion
+      wildmode = "longest:full,full", -- Command-line completion mode
+      winminwidth = 5,                -- Minimum window width
+      wrap = false,                   -- Disable wrapping of lines longer than the width of window
+      writebackup = false,            -- if a file is being edited by another program (or was written to file while editing with another program), it is not allowed to be edited
+    },
+    g = {
+      -- mapleader = " ",
+      -- maplocalleader = " ",
+      markdown_recommended_style = 0, -- Fix markdown indentation settings
+      diagnostics_enabled = true,     -- enable diagnostics at start
+      status_diagnostics_enabled = true,
+      lspkind_status_ok = true,
+      heirline_bufferline = true,
+      format_on_save = false,
+      diagnostics_mode = 3, -- set the visibility of diagnostics in the UI (0=off, 1=only show in status line, 2=virtual text off, 3=all on)
+    }
   }
 
   ---  SETTINGS  ---
-  vim.opt.spelllang:append "cjk" -- disable spellchecking for asian characters (VIM algorithm does not support it)
-  vim.opt.shortmess:append "c" -- don't show redundant messages from ins-completion-menu
-  vim.opt.shortmess:append "I" -- don't show the default intro message
+  vim.opt.spelllang:append "cjk"                   -- disable spellchecking for asian characters (VIM algorithm does not support it)
+  vim.opt.shortmess:append "c"                     -- don't show redundant messages from ins-completion-menu
+  vim.opt.shortmess:append "I"                     -- don't show the default intro message
   vim.opt.whichwrap:append "<,>,[,],h,l"
+  vim.opt.shortmess:append({ s = true, I = true }) -- disable startup message
+  vim.opt.shortmess:append({ W = true, I = true, c = true })
 
-  for k, v in pairs(default_options) do
-    vim.opt[k] = v
+
+  for scope, table in pairs(default_options) do
+    for setting, value in pairs(table) do
+      vim[scope][setting] = value
+    end
   end
 
   vim.filetype.add {
@@ -73,39 +115,13 @@ M.load_default_options = function()
       ["[jt]sconfig.*.json"] = "jsonc",
     },
   }
-
-  local default_diagnostic_config = {
-    signs = {
-      active = true,
-      values = {
-        { name = "DiagnosticSignError", text = lvim.icons.diagnostics.Error },
-        { name = "DiagnosticSignWarn", text = lvim.icons.diagnostics.Warning },
-        { name = "DiagnosticSignHint", text = lvim.icons.diagnostics.Hint },
-        { name = "DiagnosticSignInfo", text = lvim.icons.diagnostics.Information },
-      },
-    },
-    virtual_text = true,
-    update_in_insert = false,
-    underline = true,
-    severity_sort = true,
-    float = {
-      focusable = true,
-      style = "minimal",
-      border = "rounded",
-      source = "always",
-      header = "",
-      prefix = "",
-    },
-  }
-
-  vim.diagnostic.config(default_diagnostic_config)
 end
 
 M.load_headless_options = function()
-  vim.opt.shortmess = "" -- try to prevent echom from cutting messages off or prompting
-  vim.opt.more = false -- don't pause listing when screen is filled
+  vim.opt.shortmess = ""   -- try to prevent echom from cutting messages off or prompting
+  vim.opt.more = false     -- don't pause listing when screen is filled
   vim.opt.cmdheight = 9999 -- helps avoiding |hit-enter| prompts.
-  vim.opt.columns = 9999 -- set the widest screen possible
+  vim.opt.columns = 9999   -- set the widest screen possible
   vim.opt.swapfile = false -- don't use a swap file
 end
 
