c00b087931b0dee774a8920ca30e4b349d609643
Danilo Nascimento <dan.silva.13.zx@gmail.com>
2024-06-16 14:25:43 -0300

    Personal plugins

--- lua/lvim/utils.lua
+++ lua/lvim/utils.lua
@@ -129,4 +129,109 @@ function M.fs_copy(source, destination)
   end
 end
 
+--TODO: move functions below to appropriate files
+
+local root_patterns = { ".git", "lua" }
+
+---@param plugin string
+function M.has_plugin(plugin)
+  return require("lazy.core.config").plugins[plugin] ~= nil
+end
+
+-- returns the root directory based on:
+-- * lsp workspace folders
+-- * lsp root_dir
+-- * root pattern of filename of the current buffer
+-- * root pattern of cwd
+---@return string
+function M.get_root()
+  ---@type string?
+  local path = vim.api.nvim_buf_get_name(0)
+  path = path ~= "" and vim.loop.fs_realpath(path) or nil
+  ---@type string[]
+  local roots = {}
+  if path then
+    for _, client in pairs(vim.lsp.get_active_clients({ bufnr = 0 })) do
+      local workspace = client.config.workspace_folders
+      local paths = workspace and vim.tbl_map(function(ws)
+            return vim.uri_to_fname(ws.uri)
+          end, workspace) or client.config.root_dir and { client.config.root_dir } or {}
+      for _, p in ipairs(paths) do
+        local r = vim.loop.fs_realpath(p)
+        if path:find(r, 1, true) then
+          roots[#roots + 1] = r
+        end
+      end
+    end
+  end
+  table.sort(roots, function(a, b)
+    return #a > #b
+  end)
+  ---@type string?
+  local root = roots[1]
+  if not root then
+    path = path and vim.fs.dirname(path) or vim.loop.cwd()
+    ---@type string?
+    root = vim.fs.find(root_patterns, { path = path, upward = true })[1]
+    root = root and vim.fs.dirname(root) or vim.loop.cwd()
+  end
+  ---@cast root string
+  return root
+end
+
+function M.toggle_opt(option, silent, values)
+  local Util = require("lazy.core.util")
+
+  if values then
+    if vim.opt_local[option]:get() == values[1] then
+      vim.opt_local[option] = values[2]
+    else
+      vim.opt_local[option] = values[1]
+    end
+    return Util.info("Set " .. option .. " to " .. vim.opt_local[option]:get(), { title = "Option" })
+  end
+  vim.opt_local[option] = not vim.opt_local[option]:get()
+  if not silent then
+    if vim.opt_local[option]:get() then
+      Util.info("Enabled " .. option, { title = "Option" })
+    else
+      Util.warn("Disabled " .. option, { title = "Option" })
+    end
+  end
+end
+
+function M.toggle_diagnostics()
+  local diagnostics = {
+    off = {
+      underline = false,
+      virtual_text = false,
+      signs = false,
+    },
+    on = {
+      virtual_text = true,
+      underline = true,
+      severity_sort = true,
+      signs = {
+        active = true,
+        values = {
+          { name = "DiagnosticSignError", text = lvim.icons.diagnostics.Error },
+          { name = "DiagnosticSignWarn",  text = lvim.icons.diagnostics.Warning },
+          { name = "DiagnosticSignHint",  text = lvim.icons.diagnostics.Hint },
+          { name = "DiagnosticSignInfo",  text = lvim.icons.diagnostics.Information },
+        },
+      },
+    },
+  }
+  local Util = require("lazy.core.util")
+  vim.g.diagnostics_enabled = not vim.g.diagnostics_enabled
+  if vim.g.diagnostics_enabled then
+    vim.diagnostic.enable()
+    Util.info("Enabled diagnostics", { title = "Diagnostics" })
+  else
+    vim.diagnostic.disable()
+    Util.warn("Disabled diagnostics", { title = "Diagnostics" })
+  end
+  vim.diagnostic.config(diagnostics[vim.g.diagnostics_enabled and "on" or "off"])
+end
+
 return M
